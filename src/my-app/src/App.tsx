944646b3b7b93ebd7543a6c7750a6f701ce6a9eb
Vuk Kesic <vukk.98@gmail.com>
2023-03-24 15:34:46 +0100

    Home page - added home page.

--- src/my-app/src/App.tsx
+++ src/my-app/src/App.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import RegistrationForm from './UI/RegistrationForm';
+import {
+  BrowserRouter as Router,
+  Routes, //replaces "Switch" used till v5
+  Route,
+  Navigate
+} from "react-router-dom";
+import HomePage from './UI/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<RegistrationForm />} />
+          <Route path="/*" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/my-app/src/UI/HomePage.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <div className="card-columns">
+
+                <div className="card border-0">
+                    <div className="position-relative">
+
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Your health is our concern</h5>
+
+                        <p className="card-text">Ethic and professionalism are the basis of the guidelines that guide
+                            us in our daily work. Your health is our concern, your needs are first for us,
+                            and our team of skilled doctors are everyday here for you,
+                            working closely together in order to find the best treatment for you..</p>
+                    </div>
+                    <div className="card-footer">
+                        <div className="media align-items-center">
+                            <div className="media-body"><a className="card-link text-uppercase" href="javascript://">Read More</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-columns">
+
+                <div className="card border-0">
+                    <div className="position-relative">
+
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Quick scheduling</h5>
+
+                        <p className="card-text">With our team, you recieve medical tests and medical consultations
+                            in just a few days. This means a diagnosis comes fast appointment scheduling is fast and secure. We take care of you
+                            during the entire treatment. We are glad for the opportunity to help you as quick as possible.
+                        </p>
+                    </div>
+                    <div className="card-footer">
+                        <div className="media align-items-center">
+                            <div className="media-body"><a className="card-link text-uppercase" href="javascript://">Read More</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
