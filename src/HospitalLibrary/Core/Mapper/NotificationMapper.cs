007600e33bdf973fc357ddd4574078d012e78ca3
Vuk Kesic <vukk.98@gmail.com>
2023-04-11 08:50:39 +0200

    Notifications - added logic to backend

---
+++ src/HospitalLibrary/Core/Mapper/NotificationMapper.cs
@@ -0,0 +1,25 @@
+﻿using HospitalLibrary.Core.DTOs;
+using HospitalLibrary.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Mapper
+{
+    public class NotificationMapper
+    {
+        public NotificationMapper() { }
+
+        public NotificationDTO MapNotificationToNotificationDTO(Notification notification)
+        {
+            return new NotificationDTO(notification.Id, notification.Title, notification.Text);
+        }
+
+        public Notification MapNotificationDTOToNotification(NotificationDTO notificationDTO)
+        {
+            return new Notification(notificationDTO.Id, notificationDTO.Title, notificationDTO.Text);
+        }
+    }
+}
---
+++ src/HospitalLibrary/Core/Service/NotificationService.cs
@@ -0,0 +1,44 @@
+﻿using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Service
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly INotificationRepository _notificationRepository;
+        public NotificationService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
+
+        public void Create(Notification notification)
+        {
+            _notificationRepository.Create(notification);
+        }
+
+        public void Delete(Notification notification)
+        {
+            _notificationRepository.Delete(notification);
+        }
+
+        public IEnumerable<Notification> GetAll()
+        {
+            return _notificationRepository.GetAll();
+        }
+
+        public Notification GetById(int id)
+        {
+            return _notificationRepository.GetById(id);
+        }
+
+        public void Update(Notification notification)
+        {
+            _notificationRepository.Update(notification);
+        }
+    }
+}
